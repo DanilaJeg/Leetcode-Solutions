5decf9d52a6c0b9799f6dc3727cae391e940556d
DanilaJeg <150244811+DanilaJeg@users.noreply.github.com>
2024-08-19 14:53:22 +0100

    Create group-anagrams.py
    solution: https://leetcode.com/problems/group-anagrams/

---
+++ group-anagrams.py
@@ -0,0 +1,37 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        letters = {
+            "a": 0,
+            "b": 1, 
+            "c": 2, 
+            "d": 3, 
+            "e": 4, 
+            "f": 5, 
+            "g": 6, 
+            "h": 7, 
+            "i": 8,
+            "j": 9,
+            "k": 10,
+            "l": 11,
+            "m": 12,
+            "n": 13,
+            "o": 14,
+            "p": 15,
+            "q": 16,
+            "r": 17,
+            "s": 18,
+            "t": 19,
+            "u": 20,
+            "v": 21,
+            "w": 22,
+            "x": 23,
+            "y": 24,
+            "z": 25,
+        }
+        for word in strs:
+            count = [0] * 26
+            for c in word:
+                count[letters[c]] += 1
+            anagrams[tuple(count)].append(word)
+        return anagrams.values()
